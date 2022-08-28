2492bb93d37f34f121bf87fcda9a988eeaafd3f5
Alan Ramalho <alanlopes.r@gmail.com>
2022-08-28 16:04:33 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/example/demo/resources/UserResources.java
+++ src/main/java/com/example/demo/resources/UserResources.java
@@ -1,38 +1,53 @@
 package com.example.demo.resources;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.UserAPI;
 import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.Services;
 
 @RestController
 @RequestMapping(value = "/users")
 
 // CommandLineRunner serve para executar algum comando toda vez que a aplicação
-// for iniciada
-public class UserResources implements CommandLineRunner {
+// for iniciada basta extender a classe com:
+// *implements CommandLineRunner*
+public class UserResources {
 
     @Autowired
     private UserRepository userRep;
 
+    @Autowired
+    private Services services;
+
     @GetMapping
-    public ResponseEntity<UserAPI> findAll() {
-        UserAPI u = new UserAPI(1L, "Maria", "maria@gmail.com", "9999-9999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<UserAPI>> findAll() {
+        List<UserAPI> result = services.findAll();
+        return ResponseEntity.ok().body(result);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        UserAPI u1 = new UserAPI(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        UserAPI u2 = new UserAPI(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<UserAPI>> findById(@PathVariable Long id) {
+        Optional<UserAPI> result = services.findById(id);
+        return ResponseEntity.ok().body(result);
 
-        userRep.saveAll(Arrays.asList(u1, u2));
     }
+
+    // @Override
+    // public void run(String... args) throws Exception {
+    // UserAPI u1 = new UserAPI(null, "Maria Brown", "maria@gmail.com", "988888888",
+    // "123456");
+    // UserAPI u2 = new UserAPI(null, "Alex Green", "alex@gmail.com", "977777777",
+    // "123456");
+
+    // userRep.saveAll(Arrays.asList(u1, u2));
+    // }
 }
--- src/main/java/com/example/demo/services/Services.java
+++ src/main/java/com/example/demo/services/Services.java
@@ -1,8 +1,12 @@
 package com.example.demo.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entities.UserAPI;
 import com.example.demo.repositories.UserRepository;
 
 @Service
@@ -10,5 +14,14 @@ public class Services {
 
     @Autowired
     private UserRepository userRep;
+
+    public List<UserAPI> findAll() {
+        return userRep.findAll();
+    }
+
+    public Optional<UserAPI> findById(Long id) {
+        return userRep.findById(id);
+
+    }
 
 }
